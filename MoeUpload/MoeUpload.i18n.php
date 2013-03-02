db81516614e84b3aca71dda4876cdcbeed77f914
zoglun <zoglun@gmail.com>
2013-03-02 15:43:22 -0600

    Update MoeUpload.i18n.php
    排版修正

--- MoeUpload/MoeUpload.i18n.php
+++ MoeUpload/MoeUpload.i18n.php
@@ -2,34 +2,34 @@
 
 $messages = array();
 
-/* English */
-$messages['en'] = array(
-  'moemoeQCharName' => 'Character name',
-  'moemoeQAuthor' => 'Author',
-	'moemoeQSrcUrl' => 'Origin URL',
-	'moemoeQdec' => 'MoegirlPedia style uploadpage enhance.'
-);
-
 /* Chinese */
 $messages['zh-hans'] = array(
 	'moemoeQCharName' => '人物名',
-  'moemoeQAuthor' => '作者',
+	'moemoeQAuthor' => '作者',
 	'moemoeQSrcUrl' => '源地址',
 	'moemoeQdec' => '萌娘百科式上傳頁面註釋增強插件'
 );
 
 $messages['zh-hant'] = array(
 	'moemoeQCharName' => '人物名',
-  'moemoeQAuthor' => '作者',
+	'moemoeQAuthor' => '作者',
 	'moemoeQSrcUrl' => '源地址',
 	'moemoeQdec' => '萌娘百科式上傳頁面註釋增強插件'
+);
+
+/* English */
+$messages['en'] = array(
+	  'moemoeQCharName' => 'Character name',
+	  'moemoeQAuthor' => 'Author',
+	  'moemoeQSrcUrl' => 'Origin URL',
+	  'moemoeQdec' => 'MoegirlPedia style uploadpage enhance.'
 );
 
 /* Japanese */
 
 $messages['ja'] = array(
 	'moemoeQCharName' => '名前',
-  'moemoeQAuthor' => '絵師',
+	'moemoeQAuthor' => '絵師',
 	'moemoeQSrcUrl' => 'ソースアドレス',
 	'moemoeQdec' => '萌えっ娘百科形式アップロード強化する'
 );
