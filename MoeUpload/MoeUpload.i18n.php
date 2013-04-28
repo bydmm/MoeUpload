4a6e14a80f77093b547541daad06dc1e2386f1ba
baskice <zoglun@gmail.com>
2013-04-28 00:38:28 -0500

    繁简出错

--- MoeUpload/MoeUpload.i18n.php
+++ MoeUpload/MoeUpload.i18n.php
@@ -7,14 +7,14 @@ $messages['zh-hans'] = array(
 	'moemoeQCharName' => '人物名',
 	'moemoeQAuthor' => '作者',
 	'moemoeQSrcUrl' => '源地址',
-	'moemoeQdec' => '[http://wiki.moegirl.org/ 萌娘百科]式上傳頁面註釋增強插件'
+	'moemoeQdec' => '[http://zh.moegirl.org/ 萌娘百科]式上传页面增强插件'
 );
 
 $messages['zh-hant'] = array(
 	'moemoeQCharName' => '人物名',
 	'moemoeQAuthor' => '作者',
 	'moemoeQSrcUrl' => '源地址',
-	'moemoeQdec' => '[http://wiki.moegirl.org/ 萌娘百科]式上傳頁面註釋增強插件'
+	'moemoeQdec' => '[http://zh.moegirl.org/ 萌娘百科]式上傳頁面註釋增強插件'
 );
 
 /* English */
